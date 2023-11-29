9544af5fe6c2fb7463b550418e9db49652b4bcc1
LukyVj <lucas.bonomi@gmail.com>
2023-11-29 12:47:42 +0100

    update

--- src/app/components/story.tsx
+++ src/app/components/story.tsx
@@ -1,22 +1,101 @@
+"use client";
+
+import cx from "classnames";
+import Search from "./search";
+import algoliasearch from "algoliasearch/lite";
+import { InstantSearch } from "react-instantsearch";
+import { useState } from "react";
+
+const searchClient = algoliasearch(
+  "PVXYD3XMQP",
+  "69636a752c16bee55133304edea993f7"
+);
+
+const tabs = [
+  {
+    name: "Complementary reco",
+    href: "#",
+    title: "foo bar baz",
+    description: "bar baz qux",
+    storyObkect: {},
+  },
+  {
+    name: "Alternative reco",
+    href: "#",
+    title: "foo bar baz",
+    description: "bar baz qux",
+    storyObkect: {},
+  },
+  {
+    name: "Trending items",
+    href: "#",
+    title: "foo bar baz",
+    description: "bar baz qux",
+    storyObkect: {},
+  },
+  {
+    name: "Trending facets value",
+    href: "#",
+    title: "foo bar baz",
+    description: "bar baz qux",
+    storyObkect: {},
+  },
+  {
+    name: "Looking similar",
+    href: "#",
+    title: "foo bar baz",
+    description: "bar baz qux",
+    storyObkect: {},
+  },
+];
+
 const Story = () => {
+  const [currentTab, setCurrentTab] = useState(0);
+
+  const handleTabs = (e, clickedTabIndex) => {
+    e.preventDefault();
+
+    setTabs((prevTabs) =>
+      prevTabs.map((tab, index) => {
+        tab.current = index === clickedTabIndex;
+        return tab;
+      })
+    );
+  };
+
   return (
-    <div>
-      <h1>Story</h1>
+    <InstantSearch searchClient={searchClient} indexName="games">
+      <div className="w-full border border-gray-300 bg-gray-200">
+        <div className="grid grid-cols-2">
+          <div className="p-4 bg-white">
+            <Search />
+          </div>
 
-      <ul>
-        <li>Complementary recommendations</li>
-        <li>Alternative recommendations</li>
-        <li>Trending items</li>
-        <li>Trending facets value</li>
-        <li>Looking similar</li>
-      </ul>
-
-      <div>
-        <div>RENDU</div>
-
-        <div>CODE BLOCK</div>
+          <div>
+            <div className="hidden sm:block">
+              <nav className="flex space-x-4" aria-label="Tabs">
+                {tabs.map((tab, index) => (
+                  <a
+                    key={tab.name}
+                    href={tab.href}
+                    className={cx(
+                      tab.current
+                        ? "bg-indigo-100 text-indigo-700"
+                        : "text-gray-500 hover:text-gray-700",
+                      "rounded-md px-3 py-2 text-sm font-medium"
+                    )}
+                    aria-current={tab.current ? "page" : undefined}
+                    onClick={(e) => handleTabs(e, index)}
+                  >
+                    {tab.name}
+                  </a>
+                ))}
+              </nav>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
+    </InstantSearch>
   );
 };
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,10 @@ import Story from "./components/story";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Total-mother-fucking-recall</h1>
+    <main className="min-h-screen p-24">
+      <header className="text-center text-2xl font-black">
+        <h1>Total-mother-fucking-recall</h1>
+      </header>
 
       <Story />
     </main>
